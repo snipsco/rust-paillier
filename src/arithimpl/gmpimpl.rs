5225c0977c771e61f8c0873836c58fd3d2c5e12a
Morten Dahl <douglas.botley@gmail.com>
2016-10-25 23:11:41 +0200

    ..

--- src/arithimpl/gmpimpl.rs
+++ src/arithimpl/gmpimpl.rs
@@ -8,71 +8,47 @@ use super::traits::*;
 
 use self::gmp::mpz::Mpz;
 
-#[derive(Clone)]
-pub struct GmpInteger(Mpz);
+// #[derive(Clone)]
+// pub struct GmpInteger(Mpz);
 
-// impl Samplable for ramp::Int {
-//     fn sample(upper: &Self) -> Self {
-//         use self::ramp::RandomInt;
-//         let mut rng = rand::OsRng::new().unwrap();
-//         rng.gen_uint_below(upper)
-//     }
-// }
+impl Samplable for Mpz {
+    fn sample(upper: &Self) -> Self {
+        // TODO
+        Mpz::zero()
+    }
+}
 
-impl NumberTests for GmpInteger {
+impl NumberTests for Mpz {
     fn is_zero(me: &Self) -> bool { me.is_zero() }
     fn is_even(me: &Self) -> bool { me.is_multiple_of(&Mpz::from(2)) }
     fn is_negative(me: &Self) -> bool { me < &Mpz::from(0) }
 }
 
-use num_traits::{Zero, One};
+pub use num_traits::{Zero, One};
 
-impl Zero for GmpInteger {
-    fn zero() -> Self {
-        GmpInteger::zero()
+impl ModularArithmetic for Mpz {
+
+    fn modinv(a: &Self, prime: &Self) -> Self {
+        a.clone()
     }
-    fn is_zero(self: &Self) -> bool {
-        self.is_zero()
+
+    fn modpow(x: &Self, e: &Self, prime: &Self) -> Self {
+        x.clone()
     }
-}
 
-impl One for GmpInteger {
-    fn one() -> Self {
-        GmpInteger::one()
+    fn egcd(a: &Self, b: &Self) -> (Self, Self, Self) {
+        (a.clone(), a.clone(), a.clone())
     }
-}
-
-use std::ops::{Add, Sub, Mul, Rem, Div, Neg, Shr};
-impl         Add for GmpInteger { type Output=GmpInteger; }
-impl<'a, 'b> Add<&'a GmpInteger> for &'b GmpInteger {}
-impl         Sub for GmpInteger {}
-impl<'a>     Sub<&'a GmpInteger> for GmpInteger {}
-impl<'a, 'b> Sub<&'a GmpInteger> for &'b GmpInteger {}
-impl         Mul for GmpInteger {}
-impl<'b>     Mul<GmpInteger> for &'b GmpInteger {}
-impl<'a, 'b> Mul<&'a GmpInteger> for &'b GmpInteger {}
-impl<'a, 'b> Div<&'a GmpInteger> for &'b GmpInteger {}
-impl         Rem<GmpInteger> for GmpInteger {}
-impl<'a>     Rem<&'a GmpInteger> for GmpInteger { type Output=GmpInteger; }
-impl<'b>     Rem<GmpInteger> for &'b GmpInteger {}
-impl<'a, 'b> Rem<&'a GmpInteger> for &'b GmpInteger { type Output=GmpInteger; }
-impl Neg for GmpInteger {}
-impl Shr<usize> for GmpInteger {}
-
-impl ModularArithmetic for GmpInteger {
-
-    // fn modpow(x: &Self, e: &Self, prime: &Self) -> Self {
-    // }
 
 }
 
-// use std::convert::Into;
-//
-// impl ConvertFrom<Mpz> for u64 {
-//     fn _from(x: &Mpz) -> u64 {
-//         Mpz::into(x.clone())
-//         // u64::from(x)
-//     }
-// }
+impl ConvertFrom<Mpz> for u64 {
+    fn _from(x: &Mpz) -> u64 {
+        // TODO
+        0_u64
+        // Mpz::into(x.clone())
+        // u64::from(x)
+    }
+}
 
 pub type BigInteger = Mpz;
--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,9 @@ mod rampinstance
     pub type RampPlainPaillier = ::plain::AbstractPlainPaillier<RampBigInteger>;
     pub type RampPackedPaillier = ::packed::AbstractPackedPaillier<u64, RampPlainPaillier>;
 
-    pub type BigInteger = RampBigInteger;
-    pub type PlainPaillier = RampPlainPaillier;
-    pub type PackedPaillier = RampPackedPaillier;
+    // pub type BigInteger = RampBigInteger;
+    // pub type PlainPaillier = RampPlainPaillier;
+    // pub type PackedPaillier = RampPackedPaillier;
 }
 #[cfg(feature="inclramp")]
 pub use self::rampinstance::*;
@@ -43,12 +43,31 @@ mod numinstance
     pub type NumPlainPaillier = ::plain::AbstractPlainPaillier<NumBigInteger>;
     pub type NumPackedPaillier = ::packed::AbstractPackedPaillier<u64, NumPlainPaillier>;
 
-    #[cfg(not(feature="inclramp"))]
-    pub type BigInteger = NumBigInteger;
-    #[cfg(not(feature="inclramp"))]
-    pub type PlainPaillier = NumPlainPaillier;
-    #[cfg(not(feature="inclramp"))]
-    pub type PackedPaillier = NumPackedPaillier;
+    // #[cfg(not(feature="inclramp"))]
+    // pub type BigInteger = NumBigInteger;
+    // #[cfg(not(feature="inclramp"))]
+    // pub type PlainPaillier = NumPlainPaillier;
+    // #[cfg(not(feature="inclramp"))]
+    // pub type PackedPaillier = NumPackedPaillier;
 }
 #[cfg(feature="inclnum")]
 pub use self::numinstance::*;
+
+
+/**************
+  GMP instance
+ **************/
+
+#[cfg(feature="inclgmp")]
+mod gmpinstance
+{
+    pub use arithimpl::gmpimpl::BigInteger as GmpBigInteger;
+    pub type GmpPlainPaillier = ::plain::AbstractPlainPaillier<GmpBigInteger>;
+    pub type GmpPackedPaillier = ::packed::AbstractPackedPaillier<u64, GmpPlainPaillier>;
+
+    pub type BigInteger = GmpBigInteger;
+    pub type PlainPaillier = GmpPlainPaillier;
+    pub type PackedPaillier = GmpPackedPaillier;
+}
+#[cfg(feature="inclgmp")]
+pub use self::gmpinstance::*;
